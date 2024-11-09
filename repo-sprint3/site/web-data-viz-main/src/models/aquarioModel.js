110e17114feeea3eeb3833821b3aa8b77eb62fa7
guilhermeLira10 <guilherme.lira@sptech.school>
2024-11-09 10:34:37 -0300

    ajustes para a dashboard

--- repo-sprint3/site/web-data-viz-main/src/models/aquarioModel.js
+++ repo-sprint3/site/web-data-viz-main/src/models/aquarioModel.js
@@ -1,8 +1,8 @@
 var database = require("../database/config");
 
 function buscarAquariosPorEmpresa(empresaId) {
-
-  var instrucaoSql = `SELECT * FROM aquario a WHERE fk_empresa = ${empresaId}`;
+  var instrucaoSql = `SELECT * FROM estufa WHERE fkEmpresa = 1`;
+  
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
@@ -10,7 +10,7 @@ function buscarAquariosPorEmpresa(empresaId) {
 
 function cadastrar(empresaId, descricao) {
   
-  var instrucaoSql = `INSERT INTO (descricao, fk_empresa) aquario VALUES (${descricao}, ${empresaId})`;
+  var instrucaoSql = `INSERT INTO (descricao, fkEmpresa) estufa VALUES (${descricao}, ${empresaId})`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
